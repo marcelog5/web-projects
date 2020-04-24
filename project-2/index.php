375f358c5522c59e9f03a3454fef0308d41ff80d
marcelog5 <marcelovictorg2@gmail.com>
2020-04-24 09:41:12 -0300

    Created 404 page and applied dinamic header and fotter in the project-2

--- project-2/index.php
+++ project-2/index.php
@@ -41,180 +41,20 @@
         <div class="clear"></div><!--clear-->
     </header>
 
-    <section class="form-bg">
-        <div class="overlay"></div><!--overlay-->
-        
-        <div class="container">
-            <div class="form-wraper">
-                <h2>Qual o seu melhor e-mail?</h2>
-            
-                <form>
-                    <input required type="email" name="email"/>
+    <?php
 
-                    <input type="submit" name="acao" value="Cadastrar"/>
-                </form>
-            </div><!--form-wraper-->
-        </div><!--container-->
-    </section><!--form-bg-->
+        $url = isset($_GET['url']) ? $_GET['url'] : 'home';
 
-    <section class="about">
-        <div class="container">
-            <div class="w50 about-infor">
-                <h2>Lorem ipsum dolor</h2>
+        if(file_exists('pages/'.$url.'.php')){
+            include('pages/'.$url.'.php');
+        }else{
+            $page404 = true;
+            include('pages/404.php');
+        }
 
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer enim arcu,
-                    ullamcorper in odio ut, egestas tincidunt magna. 
-                    In vitae venenatis lectus. Nunc massa enim, facilisis ut mauris id, 
-                    tempor volutpat elit. Sed efficitur quam vitae ex euismod, eu 
-                    fringilla est dapibus.In imperdiet placerat justo quis porttitor. 
-                    Integer a tortor nulla. Ut et hendrerit est. Etiam fringilla neque ut 
-                    augue pellentesque pellentesque. Nulla pretium blandit eleifend. Cras tincidunt, 
-                    libero in vehicula pellentesque, sem ante rhoncus libero, 
-                    in congue odio felis volutpat leo. Aliquam eu eros luctus, auctor est sit amet, 
-                    laoreet leo. Donec nisl ante, consequat in libero sed, tempor tristique felis. 
-                    In eleifend magna quis porttitor iaculis.
-                </p>
-                    
-                <p>
-                    In imperdiet placerat justo quis porttitor. 
-                    Integer a tortor nulla. Ut et hendrerit est. Etiam fringilla neque ut 
-                    augue pellentesque pellentesque. Nulla pretium blandit eleifend. Cras tincidunt, 
-                    libero in vehicula pellentesque, sem ante rhoncus libero, 
-                    in congue odio felis volutpat leo. Aliquam eu eros luctus, auctor est sit amet, 
-                    laoreet leo. Donec nisl ante, consequat in libero sed, tempor tristique felis. 
-                    In eleifend magna quis porttitor iaculis.
-                </p>
-            </div><!--about-infor-->
+    ?>
 
-            <div class="w50 img-wraper">
-                <img src="<?php echo INCLUDE_PATH;?>images/dogao.jpeg"/>
-            </div><!--img-wraper-->
-            <div class="clear"></div><!--clear-->
-        </div><!--container-->
-    </section><!--about-->
-
-    <section class="specialties">
-        <div class="container">
-            <h2>Minha especialidades</h2>
-
-            <div class="technology-wraper">
-                <div class="technology-logo"></div>
-
-                <h2>CSS3</h2>
-
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer enim arcu,
-                    ullamcorper in odio ut, egestas tincidunt magna. 
-                    In vitae venenatis lectus. Nunc massa enim, facilisis ut mauris id, 
-                    tempor volutpat elit. Sed efficitur quam vitae ex euismod, eu 
-                    fringilla est dapibus. In imperdiet placerat justo quis porttitor. 
-                    Integer a tortor nulla. Ut et hendrerit est. Etiam fringilla neque ut 
-                    augue pellentesque pellentesque. 
-                </p>
-            </div><!--technology-wraper-->
-
-            <div class="technology-wraper">
-                <div class="technology-logo"></div>
-
-                <h2>HTML5</h2>
-
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer enim arcu,
-                    ullamcorper in odio ut, egestas tincidunt magna. 
-                    In vitae venenatis lectus. Nunc massa enim, facilisis ut mauris id, 
-                    tempor volutpat elit. Sed efficitur quam vitae ex euismod, eu 
-                    fringilla est dapibus. In imperdiet placerat justo quis porttitor. 
-                    Integer a tortor nulla. Ut et hendrerit est. Etiam fringilla neque ut 
-                    augue pellentesque pellentesque. 
-                </p>
-            </div><!--technology-wraper-->
-
-            <div class="technology-wraper">
-                <div class="technology-logo"></div>
-
-                <h2>JavaScript</h2>
-
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer enim arcu,
-                    ullamcorper in odio ut, egestas tincidunt magna. 
-                    In vitae venenatis lectus. Nunc massa enim, facilisis ut mauris id, 
-                    tempor volutpat elit. Sed efficitur quam vitae ex euismod, eu 
-                    fringilla est dapibus. In imperdiet placerat justo quis porttitor. 
-                    Integer a tortor nulla. Ut et hendrerit est. Etiam fringilla neque ut 
-                    augue pellentesque pellentesque. 
-                </p>
-            </div><!--technology-wraper-->
-            <div class="clear"></div><!--clear-->
-        </div><!--container-->
-    </section><!--specialties-->
-
-    <section class="testimony-services">
-        <div class="container">
-            <div class="testimony w50">
-                <h2>Depoimento dos nossos clientes</h2>
-
-                <div class="testimony-single">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer enim arcu,
-                        ullamcorper in odio ut, egestas tincidunt magna. 
-                        In vitae venenatis lectus. Nunc massa enim, facilisis ut mauris id, 
-                        tempor volutpat elit. Sed efficitur quam vitae ex euismod, eu 
-                        fringilla est dapibus. In imperdiet placerat justo quis porttitor. 
-                    </p>
-
-                    <h2>Lorem Ipsum</h2>
-                </div><!--testimony-single-->
-
-                <div class="testimony-single">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer enim arcu,
-                        ullamcorper in odio ut, egestas tincidunt magna. 
-                        In vitae venenatis lectus. Nunc massa enim, facilisis ut mauris id, 
-                        tempor volutpat elit. Sed efficitur quam vitae ex euismod, eu 
-                        fringilla est dapibus. In imperdiet placerat justo quis porttitor. 
-                    </p>
-
-                    <h2>Lorem Ipsum</h2>
-                </div><!--testimony-single-->
-            </div><!--testimony-->
-
-            <div class="services w50">
-                <h2>Serviços</h2>
-
-                <ul>
-                    <li>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer enim arcu,
-                        ullamcorper in odio ut, egestas tincidunt magna. 
-                        In vitae venenatis lectus.
-                    </li>
-                    <li>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer enim arcu,
-                        ullamcorper in odio ut, egestas tincidunt magna. 
-                        In vitae venenatis lectus.
-                    </li>
-                    <li>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer enim arcu,
-                        ullamcorper in odio ut, egestas tincidunt magna. 
-                        In vitae venenatis lectus.
-                    </li>
-                    <li>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer enim arcu,
-                        ullamcorper in odio ut, egestas tincidunt magna. 
-                        In vitae venenatis lectus.
-                    </li>
-                    <li>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer enim arcu,
-                        ullamcorper in odio ut, egestas tincidunt magna. 
-                        In vitae venenatis lectus.
-                    </li>
-                </ul>
-            </div><!--services-->
-            <div class="clear"></div><!--clear-->
-        </div><!--container-->
-    </section><!--testimony-services-->
-
-    <footer>
+    <footer <?php if(isset($page404) && $page404 == true) echo 'class="fixed"'?>>
         <div class="container">
             <p>Todos os direitos reservados</p>
         </div><!--container-->
---
+++ project-2/pages/404.php
@@ -0,0 +1,9 @@
+<section class="error-404">
+    <div class="container">
+        <div class="wraper-404">
+            <h2>Página não existe!</h2>
+
+            <p>Deseja voltar para a <a href="<?php echo INCLUDE_PATH;?>">página inicial</a>?</p>
+        </div>
+    </div><!--container-->
+</section><!--error-404-->
