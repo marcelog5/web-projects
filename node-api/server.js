130e477739f3568043c4f8a6fba09fec8385656e
marcelog5 <marcelovictorg2@gmail.com>
2020-05-29 18:28:00 -0300

    Added initial files to project node-api

---
+++ node-api/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+app.listen(3001);
