e428b5abf1aec6231d2d08058fb9f8d95cb3737e
marcelog5 <marcelovictorg2@gmail.com>
2020-05-30 09:47:24 -0300

    restructured files in project node-api

--- node-api/server.js
+++ node-api/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 const app = express();
 
 mongoose.connect(
     'mongodb://localhost:27017/nodeapi',
-    { useNewUrlParser: true, useUnifiedTopology: true });
+    { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
-});
+requireDir('./src/models');
+
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
