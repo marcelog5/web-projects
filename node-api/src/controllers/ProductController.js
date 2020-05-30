4cf95e1e6dd8ff0797f7804d61f1b9c36085d1de
marcelog5 <marcelovictorg2@gmail.com>
2020-05-30 11:24:13 -0300

    Created CRUD in project node-api

--- node-api/src/controllers/ProductController.js
+++ node-api/src/controllers/ProductController.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const Product = mongoose.model('Products');
+const Product = mongoose.model('Product');
 
 module.exports = {
     async index(req, res) {
@@ -8,4 +8,24 @@ module.exports = {
 
         return res.json(products);
     },
+    async show(req, res) {
+        const product = await Product.findById(req.params.id);
+
+        return res.json(product);
+    },
+    async store(req, res) {
+        const product = await Product.create(req.body);
+
+        return res.json(product);
+    },
+    async update(req, res) {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+        return res.json(product);
+    },
+    async destroy(req, res) {
+        await Product.findByIdAndRemove(req.params.id);
+
+        return res.send();
+    }
 };
